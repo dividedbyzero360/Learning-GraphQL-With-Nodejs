9a7707006051d6a45a20b8c221fda60b83367935
Wastabir Rajvee Alam <wastabirrajveealamsa_cse10@its.edu.in>
2019-01-10 05:12:31 -0500

    Added the queries for server2

--- server2.js
+++ server2.js
@@ -7,6 +7,9 @@ var schema = buildSchema(`
         course(id: Int!): Course
         courses(topic: String): [Course]
     },
+    type Mutation {
+        updateCourseTopic(id: Int!, topic: String!): Course
+    },
     type Course {
         id: Int
         title: String
@@ -57,9 +60,20 @@ var getCourses = function(args) {
         return coursesData;
     }
 }
+
+var updateCourseTopic = function({id, topic}) {
+    coursesData.map(course => {
+        if (course.id === id) {
+            course.topic = topic;
+            return course;
+        }
+    });
+    return coursesData.filter(course => course.id === id) [0];
+}
 var root = {
     course: getCourse,
-    courses: getCourses
+    courses: getCourses,
+    updateCourseTopic:updateCourseTopic
 };
 // Create an express server and a GraphQL endpoint
 var app = express();
